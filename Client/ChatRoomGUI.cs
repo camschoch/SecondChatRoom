69d9c4ea1d09033ed57bb413a2cd89fabcf15e8c
Your Name <you@example.com>
2017-08-25 11:25:21 -0500

    Working on client side

--- Client/ChatRoomGUI.cs
+++ Client/ChatRoomGUI.cs
@@ -8,13 +8,49 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Client
 {
     public partial class ChatRoomGUI : Form
     {
-        public ChatRoomGUI()
+        Client client;
+        public ChatRoomGUI(Client client)
         {
+            this.client = client;
             InitializeComponent();
+        }
+
+        
+        private void Send_Click(object sender, EventArgs e)
+        {
+            client.Send();
+
+            }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ConvoBox_TextChanged(object sender, EventArgs e)
+        {
+            client.Recieve();
+        }
+
+        private void PeopleOnChat_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+           
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckOnline_CheckedChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
